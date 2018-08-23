2c0f415d3b08b705eab96135573bb5fa0d53e6ed
ruslangm88 <ruslan.guerra88@gmail.com>
2018-08-22 21:59:20 -0300

    nothing

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2530,7 +2530,7 @@ class Welcome extends CI_Controller {
         $this->load->model('class/system_config');
         $GLOBALS['sistem_config'] = $this->system_config->load();
         $client_id = $GLOBALS['sistem_config']->CLIENT_ID_TOPAZIO;        
-        $API_token = "c5f678f6-4ebf-3ad8-8e96-62a483e94761";//$this->get_topazio_API_token();
+        $API_token = $this->get_topazio_API_token();
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "http://apihlg-topazio.sensedia.com/emd/v1/conciliations/".$date);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
