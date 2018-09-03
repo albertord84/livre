f85284ab7bab28896937987941205ee7aa833096
ruslangm88 <ruslan.guerra88@gmail.com>
2018-09-03 06:57:26 -0300

    nothing

--- application/libraries/Gmail.php
+++ application/libraries/Gmail.php
@@ -146,8 +146,11 @@
             $name = urlencode($name);
             $lang = $GLOBALS['sistem_config']->LANGUAGE;            
             //$this->mail->msgHTML(@file_get_contents("https://" . $_SERVER['SERVER_NAME'] . "/livre/resources/emails/email-fotos-recusadas.php?name=$name&link=$link"), dirname(__FILE__));
-            $this->mail->AddEmbeddedImage(realpath('../../../assets/img/icones/09 - fotos erradas.png'), "logo_09", "passos.png", "base64", "image/png");
-            $this->mail->Body = $this->curl_get_contents("https://" . $_SERVER['SERVER_NAME'] . "/livre/resources/emails/email-fotos-recusadas.php?name=$name&link=$link");            
+            //$this->mail->AddEmbeddedImage(realpath('../../../assets/img/icones/09 - fotos erradas.png'), "logo_09", "passos.png", "base64", "image/png");
+            //$this->mail->Body = $this->curl_get_contents("https://" . $_SERVER['SERVER_NAME'] . "/livre/resources/emails/email-fotos-recusadas.php?name=$name&link=$link");            
+            ob_start();
+            include "https://" . $_SERVER['SERVER_NAME'] . "/livre/resources/emails/email-fotos-recusadas.php?name=$name&link=$link";
+            $this->mail->Body = ob_get_clean();
             if (!$this->mail->send()) {
                 $result['success'] = false;
                 $result['message'] = "Mailer Error: " . $this->mail->ErrorInfo;
