8a3b3b8d53ee14201b57320378b8254e403dd24e
ruslangm88 <ruslan.guerra88@gmail.com>
2018-09-03 00:36:57 -0300

    https

--- robots/robot_check_contracts.sh
+++ robots/robot_check_contracts.sh
@@ -4,4 +4,4 @@ date=$(date +%Y%m%d)
 
 now=$(date +"%T")
 
-curl http://localhost/livre/index.php/welcome/robot_checking_contracts >> /opt/lampp/htdocs/livre/log/contracts-${date}.log
+curl https://localhost/livre/index.php/welcome/robot_checking_contracts >> /opt/lampp/htdocs/livre/log/contracts-${date}.log
