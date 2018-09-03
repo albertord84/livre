792bbd08a00fc18dfbcd796fd2ed4d1d0785faa8
ruslangm88 <ruslan.guerra88@gmail.com>
2018-09-03 03:25:19 -0300

    nothing

--- robots/robot_check_contracts.sh
+++ robots/robot_check_contracts.sh
@@ -4,4 +4,4 @@ date=$(date +%Y%m%d)
 
 now=$(date +"%T")
 
-curl -k http://localhost/livre/index.php/welcome/robot_checking_contracts >> /opt/lampp/htdocs/livre/log/contracts-${date}.log
+curl -k https://localhost/livre/index.php/welcome/robot_checking_contracts >> /opt/lampp/htdocs/livre/log/contracts-${date}.log
--- robots/robot_conciliation.sh
+++ robots/robot_conciliation.sh
@@ -4,4 +4,4 @@ date=$(date +%Y%m%d)
 
 now=$(date +"%T")
 
-curl -k http://localhost/livre/index.php/welcome/robot_conciliation >> /opt/lampp/htdocs/livre/log/conciliation-${date}.log
+curl -k https://localhost/livre/index.php/welcome/robot_conciliation >> /opt/lampp/htdocs/livre/log/conciliation-${date}.log
