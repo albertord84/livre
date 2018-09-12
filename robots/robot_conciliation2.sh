e6dd26877fed415c611a3b94add2489ff66e7794
ruslangm88 <ruslan.guerra88@gmail.com>
2018-09-12 18:10:53 -0300

    nothing

--- robots/robot_conciliation2.sh
+++ robots/robot_conciliation2.sh
@@ -4,5 +4,5 @@ date=$(date +%Y%m%d)
 
 now=$(date +"%T")
 
-curl -k http://localhost/livre/index.php/welcome/robot_conciliation2 >> /opt/lampp/htdocs/livre/log/conciliation-${date}.log
+curl http://localhost/livre/index.php/welcome/robot_conciliation2 >> /opt/lampp/htdocs/livre/log/conciliation-${date}.log
 
