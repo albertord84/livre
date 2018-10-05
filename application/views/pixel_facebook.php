e3ec953bf10d3bed5c5c8fe572285fd0e91045e9
josergm86 <josergm86@gmail.com>
2018-10-05 15:07:36 -0300

    a

--- application/views/pixel_facebook.php
+++ application/views/pixel_facebook.php
@@ -11,9 +11,9 @@
     fbq('init', '165606677466927');
     fbq('track', 'PageView');
 </script>
-<noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=165606677466927&ev=PageView&noscript=1"
-/></noscript>
+<noscript>
+    <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=165606677466927&ev=PageView&noscript=1"/>
+</noscript>
 <!-- End Facebook Pixel Code -->
 
 
