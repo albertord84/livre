bfa2a6b873b7c05b0caccb3e1c58c2396b5d1d17
ruslangm88 <ruslan.guerra88@gmail.com>
2018-11-01 23:56:32 -0300

    a

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4981,7 +4981,7 @@ class Welcome extends CI_Controller {
         if($transaction['payment_source'] == payment_manager::IUGU)
             $result = $this->refund_bill_iugu($id);
         else{
-            $result = ['success' => false, 'message' => 'Transação não pode ser estornada T'];
+            $result = ['success' => false, 'message' => 'Transação não pode ser estornada T'];return $result;
             if($transaction['payment_source'] == payment_manager::BRASPAG)
                 $result = $this->do_braspag_devolution($id);            
             }
